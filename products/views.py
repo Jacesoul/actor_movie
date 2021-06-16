977786534cc8783dfe46c72a18ded6c10668a7c2
Jacesoul <75877363+Jacesoul@users.noreply.github.com>
2021-06-16 13:39:35 +0900

    ADD : another class for movie title of GET method

--- products/views.py
+++ products/views.py
@@ -7,16 +7,36 @@ from django.views import View
 from products.models import *
 
 
+# class ProductsView(View):
+#     def get(self, request):
+#         actors = Actor.objects.all()
+#         results = []
+#         for actor in actors:
+#             movie_list = [x.movie.title for x in actor.actormovie_set.all()]
+#             results.append(
+#                 {
+#                     "first_name": actor.first_name,
+#                     "last_name": actor.last_name,
+#                     "date_of_birth": actor.date_of_birth,
+#                     "movie_title": movie_list,
+#                 }
+#             )
+#         return JsonResponse({'results': results}, status=200)
+
+
 class ProductsView(View):
     def get(self, request):
-        actors = Actor.objects.all()
+        movies = Movie.objects.all()
         results = []
-        for actor in actors:
+        for movie in movies:
+            actor_firstname = [
+                x.actor.first_name for x in movie.actormovie_set.all()]
             results.append(
                 {
-                    "first_name": actor.first_name,
-                    "last_name": actor.last_name,
-                    "date_of_birth": actor.date_of_birth
+                    "title": movie.title,
+                    "release_date": movie.release_date,
+                    "running_time": movie.running_time,
+                    "actor_firstName": actor_firstname,
                 }
             )
         return JsonResponse({'results': results}, status=200)
