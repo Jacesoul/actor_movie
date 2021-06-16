10271ddd30b9ef163d2f3ba278161a97de342b3b
Jacesoul <75877363+Jacesoul@users.noreply.github.com>
2021-06-16 11:57:26 +0900

    ADD : get 호출 완료 - models.py 작성완료 - c.r.u.d 구성 완료

--- products/views.py
+++ products/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
 # Create your views here.
+import json
+from django.http import JsonResponse
+from django.views import View
+from products.models import *
+
+
+class ProductsView(View):
+    def get(self, request):
+        actors = Actor.objects.all()
+        results = []
+        for actor in actors:
+            results.append(
+                {
+                    "first_name": actor.first_name,
+                    "last_name": actor.last_name,
+                    "date_of_birth": actor.date_of_birth
+                }
+            )
+        return JsonResponse({'results': results}, status=200)
